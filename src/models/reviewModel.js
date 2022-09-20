dbee21d8b4552d4ae5826168f91b0f4a0a2551a5
Nisha282/training <sahunishaji1220@gmail.com>
2022-09-20 13:32:40 +0530

    rm

---
+++ src/models/reviewModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const reviewSchema = new mongoose.Schema(
+    {
+
+        bookId: {
+            type: ObjectId,
+            required: true,
+            unique: true,
+            ref: "book" ,
+        },
+        reviewedBy:
+        {
+            type: String,
+            require: true,
+            default:'Guest',
+            value: {
+                type : String
+            }  },
+
+        reviewedAt:
+        {
+            type: Date,
+            unique: true
+        },
+        rating:
+        {
+            type: number,
+            required : true
+        },
+
+        review:
+            { type: String },
+        isDeleted:
+        {
+            type: boolean,
+            default: false
+        },
+
+       timestamps : true ,
+    })
+
+    module,exports = mongoose.model("review" , reviewSchema)
