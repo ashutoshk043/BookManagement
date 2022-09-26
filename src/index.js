fb27e15e79ff4785af2ac848b403018d4a32504e
Ashutosh Kumar <ashutoshk043@gmail.com>
2022-09-26 20:10:19 +0530

    changed Branch Name

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const {default:mongoose} = require("mongoose");
-const routes = require('./routes/route.js');
+const bodyParser = require("body-parser");
+const { default: mongoose } = require("mongoose");
+const routes = require("./routes/route.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect(
+    "mongodb+srv://SudeepKumar874:cTbGUg6fWzq3zyfB@cluster0.ks18sd9.mongodb.net/group39Database-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://SudeepKumar874:cTbGUg6fWzq3zyfB@cluster0.ks18sd9.mongodb.net/group39Database-DB?retryWrites=true&w=majority",
-{
-    useNewUrlParser: true
-}).then(() => console.log("MongoDb is connected"))
-.catch( err => console.log(err) )
+app.use("/", routes);
 
-
-  app.use('/', routes);
-
-  app.use('/', function(req, res){
-    res.status(400).send({status:false, message: "Please Enter Valid Path Or Parameters !!!!"})
-})
+app.use("/", function (req, res) {
+  res
+    .status(400)
+    .send({
+      status: false,
+      message: "Please Enter Valid Path Or Parameters !!!!",
+    });
+});
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
